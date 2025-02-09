623f2c5ec5db692f7ff1dd974cc28358b47218f0
failamir <38488480+failamir@users.noreply.github.com>
2025-02-10 02:08:53 +0700

    Update header.blade.php

--- core/resources/views/templates/basic/partials/header.blade.php
+++ core/resources/views/templates/basic/partials/header.blade.php
@@ -1,5 +1,5 @@
 @php
-$content = getContent('contact.content', true);
+    $content = getContent('contact.content', true);
 @endphp
 <!-- Header Section Starts Here -->
 
@@ -23,24 +23,27 @@ $content = getContent('contact.content', true);
             <div class="right-content d-flex flex-wrap" style="gap:10px">
                 <div>
                     <select class="langSel form--control">
-                        @foreach($language as $item)
-                        <option value="{{$item->code}}" @if(session('lang')==$item->code) selected @endif>{{ __($item->name) }}</option>
+                        @foreach ($language as $item)
+                            <option value="{{ $item->code }}" @if (session('lang') == $item->code) selected @endif>
+                                {{ __($item->name) }}</option>
                         @endforeach
                     </select>
                 </div>
                 @guest
-                <ul class="header-login">
-                    <li><a class="sign-in" href="{{ route('user.login') }}"><i class="fas fa-sign-in-alt"></i>@lang('Sign In')</a></li>
-                    <li>/</li>
-                    <li><a class="sign-up" href="{{ route('user.register') }}"><i class="fas fa-user-plus"></i>@lang('Sign Up')</a></li>
-                </ul>
+                    <ul class="header-login">
+                        <li><a class="sign-in" href="{{ route('user.login') }}"><i
+                                    class="fas fa-sign-in-alt"></i>@lang('Sign In')</a></li>
+                        <li>/</li>
+                        <li><a class="sign-up" href="{{ route('user.register') }}"><i
+                                    class="fas fa-user-plus"></i>@lang('Sign Up')</a></li>
+                    </ul>
                 @endguest
                 @auth
-                <ul class="header-login">
-                    <li>
-                        <a href="{{ route('user.home') }}">@lang('Dashboard')</a>
-                    </li>
-                </ul>
+                    <ul class="header-login">
+                        <li>
+                            <a href="{{ route('user.home') }}">@lang('Dashboard')</a>
+                        </li>
+                    </ul>
                 @endauth
             </div>
         </div>
@@ -51,28 +54,28 @@ $content = getContent('contact.content', true);
         <div class="header-bottom-area">
             <div class="logo">
                 <a href="{{ route('home') }}">
-                    <img src="{{ getImage(imagePath()['logoIcon']['path'].'/logo.png') }}" alt="@lang('Logo')">
+                    <img src="{{ getImage(imagePath()['logoIcon']['path'] . '/logo.png') }}" alt="@lang('Logo')">
                 </a>
             </div> <!-- Logo End -->
             <ul class="menu">
                 <li>
-                    <a href="{{ route('home') }}">@lang('Home')</a>
+                    <a href="{{ route('home') }}">@lang('Beranda')</a>
                 </li>
-                @foreach($pages as $k => $data)
-                <li>
-                    <a href="{{route('pages',[$data->slug])}}">{{__($data->name)}}</a>
-                </li>
+                @foreach ($pages as $k => $data)
+                    <li>
+                        <a href="{{ route('pages', [$data->slug]) }}">{{ __($data->name) }}</a>
+                    </li>
                 @endforeach
 
                 <li>
                     <a href="{{ route('blog') }}">@lang('Blog')</a>
                 </li>
                 <li>
-                    <a href="{{ route('contact') }}">@lang('Contact')</a>
+                    <a href="{{ route('contact') }}">@lang('Kontak')</a>
                 </li>
             </ul>
             <div class="d-flex flex-wrap algin-items-center">
-                <a href="{{ route('ticket') }}" class="cmn--btn btn--sm">@lang('Buy Tickets')</a>
+                <a href="{{ route('ticket') }}" class="cmn--btn btn--sm">@lang('Beli Tiket')</a>
                 <div class="header-trigger-wrapper d-flex d-lg-none ms-4">
                     <div class="header-trigger d-block d-lg-none">
                         <span></span>
@@ -89,12 +92,12 @@ $content = getContent('contact.content', true);
 <!-- Header Section Ends Here -->
 
 @push('script')
-<script>
-    $(document).ready(function() {
-        "use strict";
-        $(".langSel").on("change", function() {
-            window.location.href = "{{route('home')}}/change/" + $(this).val();
+    <script>
+        $(document).ready(function() {
+            "use strict";
+            $(".langSel").on("change", function() {
+                window.location.href = "{{ route('home') }}/change/" + $(this).val();
+            });
         });
-    });
-</script>
+    </script>
 @endpush
